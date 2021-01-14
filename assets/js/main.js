8f1335d5d919e8aac710ee112fc3842534a7fee6
muraddagha <muradha@code.edu.az>
2021-01-14 14:58:09 +0400

    davay da

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,8 @@ let menuBtn = document.querySelector(".navbar-toggle");
 
 menuBtn.addEventListener("click", () => {
   document.querySelector(".toggle-menu").classList.toggle("active");
-  document.querySelector("body").classList.toggle("modal-openn");
-  document.querySelector("html").classList.toggle("modal-openn");
+  document.querySelector("body").classList.toggle("block-scroll");
+  document.querySelector("html").classList.toggle("block-scroll");
   menuBtn.parentNode.classList.toggle("container");
   menuBtn.parentNode.classList.toggle("container-fluid");
 });
